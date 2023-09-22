2e1fea26d0e6336f3f875339e0ec29d14d26793b
graham-phillips <graham.phillips@paddypowerbetfair.com>
2023-09-22 13:56:49 +0100

    blank app

---
+++ javaapp-application/src/test/java/com/javaapplication/javaappapplication/ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.javaapplication.javaappapplication;
+
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTests {
+    @Test
+    public void FirstTest()
+    {
+        System.out.println("test");
+    }
+}
